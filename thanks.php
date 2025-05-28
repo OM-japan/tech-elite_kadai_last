fcaa61f19751fab67de8f07f69f609dfe6c7247a
OM-japan <ogawama@freeeng.net>
2025-05-28 15:34:59 +0900

    new repair

--- thanks.php
+++
@@ -1,7 +0,0 @@
-<?php include ( dirname(__FILE__) . '/header.php' ); ?>
-
-<main>
-    <h1 class="index2-4" style="height: 100px; margin-top:150px">お問い合わせありがとうございました。</h1>
-</main>
-
-<?php include ( dirname(__FILE__) . '/footer.php'); ?>  
